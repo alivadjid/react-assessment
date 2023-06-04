9fbb98ffb5bf83fc010494a3b51752025a5c750a
alivadjid <snub01@mail.ru>
2023-06-05 01:12:34 +0300

    customize combobox

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { CustomCombobox } from "./components/CustomCombobox.jsx";
 
@@ -9,15 +7,8 @@ export const App = () => {
 
   return (
     <>
-      <div>
-        <CustomCombobox />
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <CustomCombobox />
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/components/CustomCombobox.jsx
+++ src/components/CustomCombobox.jsx
@@ -1,9 +1,30 @@
 import "./CustomCombobox.css";
+// import { mdiChevronDown } from "@mdi/js";
 
 export const CustomCombobox = () => {
+  const data = ["🍎 Apple", "🍌 Banana", "🫐 Blueberry", "🥭 Mango"];
   return (
     <>
-      <div className="customCombobox">CustomComponent</div>
+      <div className="customCombobox">
+        <div className="comboboxInputRow">
+          <input
+            type="text"
+            placeholder="Choose a fruit:"
+            className="comboboxInput"
+          />
+          <img
+            src="src/assets/icons8-down-24.png"
+            alt="down"
+            style={{ width: "10px", height: "7px", padding: "13px" }}
+          />
+        </div>
+
+        <div className="datalist">
+          {data.map((element, index) => (
+            <div key={index}>{element}</div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
