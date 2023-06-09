6f57d5b098a9b151934cc89bf23ea0ae72df4468
alivadjid <snub01@mail.ru>
2023-06-09 04:07:54 +0300

    app test

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,11 +1,12 @@
-import { describe, it } from "vitest";
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import { App } from "./App";
 
 describe("App", () => {
-  it("renders headline", () => {
+  it("shows CustomCombobox", () => {
     render(<App />);
-    screen.debug();
+
+    expect(screen.findByText("customCombobox")).toBeDefined();
   });
 });
